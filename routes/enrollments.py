9c6a94f5dd0009cd880fa712d7dfbd742a642a38
al222210651@gmail.com <al222210651@gmail.com>
2026-04-27 08:13:52 -0600

    corriguiendo documentacion swagger

--- routes/enrollments.py
+++ routes/enrollments.py
@@ -4,6 +4,14 @@ from models.student import Student
 def register_routes(app):
     @app.route('/enrollments', methods=['GET'])
     def get_enrollments():
+        """Lista todos los alumnos con sus cursos (ENDPOINT CLAVE)
+        ---
+        responses:
+          200:
+            description: Alumnos con sus cursos
+            examples:
+              application/json: [{"student": "Ana García", "courses": ["Python Basics", "Flask API"]}]
+        """
         students = Student.query.all()
         result = []
         for student in students:
